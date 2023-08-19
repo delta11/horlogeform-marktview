43577b3a6e4b0a6b00f5c9be07100ea6c9f1f57f
Thomas van Putten <thomas@bit-academy.nl>
2023-08-19 19:35:57 +0200

    Fix some styling and add favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,14 +25,14 @@
     max-width: 280px;
     max-height: 300px;
   }
-  .card {
+  .watch-card {
     height: 440px;
   }
 </style>
 
-<div class="grid place-items-center 2xl:grid-cols-13 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-1 md:gap-6 gap-4">
+<div class="grid place-items-center 2xl:grid-cols-13 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-1 sm:grid-cols-1 md:gap-y-6 md:gap-x-8 gap-y-4 gap-x-6">
     {#each watches as watch}
-        <a target="_blank" href="{watch.url}" rel="noreferrer" class="card card-hover p-4 w-80
+        <a target="_blank" href="{watch.url}" rel="noreferrer" class="card watch-card card-hover p-4 w-80
         underline text-blue-600 hover:text-blue-800 visited:text-purple-600
         dark:text-gray-50 dark:hover:text-gray-200 dark:visited:text-purple-300">
             <header class="card-header max-h-[200px] flex justify-between">
@@ -45,7 +45,7 @@
                 <div class="[&>*]:pointer-events-none" use:popup={watch.popupSettings}>
                     <img src="{watch.pictureUrl}" class="watch-image" loading="lazy"
                          alt="Picture for {watch.name}"/>
-                    <div class="card p-4 variant-filled-secondary" data-popup="{watch.popupSettings.target}">
+                    <div class="p-4 variant-filled-secondary h-fit" data-popup="{watch.popupSettings.target}">
                         <img src="{watch.pictureUrl}" class="max-w-xl max-h-l" alt="Picture for {watch.name}"/>
                     </div>
                 </div>
